bb9c0879b12e66e3ffa9466232dc7836075daa0f
Kon <ktheofilis@gmail.com>
2023-02-20 13:44:56 +0200

    bug fixes and TEXT changes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,15 @@ import React from 'react';
 import Typewriter from 'typewriter-effect';
 import { salute } from '../../utils/common';
 import cmd from '../../resources/cmd.png'
+import {useEffect, useRef, useState, useMemo} from 'react';
 
 export default function Home() {
+  
+  
+  const ref1 = useRef(null);
+  const isInViewport1 = useIsInViewport(ref1);
+
+
 
   return (
     <div className='Home' id='home'>
@@ -12,9 +19,11 @@ export default function Home() {
 
       <div className='centered div'>
         <h1 id='my-name'>Konstantinos Theofilis</h1>
-        <h2 id='my-sub'>C:\Users\Software Engineer</h2>
+        <h2 ref={ref1} id='my-sub'>C:\Users\Software Engineer</h2>
+        { isInViewport1 
+        ? 
         <Typewriter
-        id='Typewriter'
+          id='Typewriter'
           options={{
                 strings: salute,
                 autoStart: true,
@@ -24,7 +33,32 @@ export default function Home() {
                 delay: 30
               }}
             />
+            : 
+            <div/>
+            }
       </div>
     </div>
   );
+}
+
+function useIsInViewport(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting),
+      ),
+    [],
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
 }
--- src/utils/common.js
+++ src/utils/common.js
@@ -2,8 +2,7 @@
 export const salute = [
     "Welcome to my personal website, where technology meets creativity!",
     "I am a Junior Software Engineer skilled in C# and JavaScript, eager to constantly learn and add skills to my toolbox and keep my knowledge up to date.",
-    "From the latest programming languages to the newest tech tools, I've got you covered, my dude. But it's not just about the tech, it's about creating things that makes people say, \"holy s**t, this is amazing!\" And that's what I strive for every damn day.",
-    "Listen, being a software engineer doesn't mean you have to be a boring son of a b**ch.", 
+    "Listen, being a software engineer doesn't mean you have to be that boring.", 
     "Now keep scrolling...", 
     "Go please...", 
     "Comeeee on...", 
