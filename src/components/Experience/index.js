aff73fb4bbee002d1417b972259e649af4587636
Kon <ktheofilis@gmail.com>
2023-03-16 16:03:22 +0200

    Adding details in Experience page. UI fixes

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -11,7 +11,9 @@ export default function Experience() {
 
   return (
     <div className='Experience' id='experience'>
+      
       <img alt='3dpctr' className='rocketImage' src={rocket}/>
+      
       <div className='centered-exp'>
         <h1>Work experience</h1>
         <h5>Adventures in techland - a memoir</h5>
@@ -24,6 +26,9 @@ export default function Experience() {
               <h3 id='work-title'>Associate Research Engineer</h3>
               <h4 id='work-sub'><a href='https://www.di.uoa.gr' target='_black' rel="noreferrer"> National and Kapodistrian University of Athens<br/>Department of Informatics and Telecommunications </a></h4>
               <h4 id='date'>Jan 2023 - Present</h4>
+              <br/>
+              <h4 id='date'>● Digital Twins Development</h4>
+              <h4 id='date'>● Unity Development</h4>
             </div>
           </div>
         </div>
@@ -36,6 +41,9 @@ export default function Experience() {
               <h3 id='work-title'>Associate Software Engineer</h3>
               <h4 id='work-sub'><a href='https://www.dsphere.io' target='_black' rel="noreferrer">dSphere.io</a></h4>
               <h4 id='date'>Sep 2022 - Dec 2022 </h4>
+              <br/>
+              <h4 id='date'>● Unity NFT Markeplace implementation</h4>
+              <h4 id='date'>● Unity Development</h4>
             </div>
           </div>
         </div>
@@ -48,6 +56,10 @@ export default function Experience() {
               <h3 id='work-title'>Associate Software Engineer</h3>
               <h4 id='work-sub'><a href='https://wappier.com' target='_black' rel="noreferrer">wappier: Intelligent Revenue Management</a></h4>
               <h4 id='date'>Dec 2020 - Sep 2022</h4>
+              <br/>
+              <h4 id='date'>● Automation Testing with Node.js</h4>
+              <h4 id='date'>● In company testing application with Unity</h4>
+              <h4 id='date'>● Unity Development</h4>
             </div>
           </div>
         </div>
